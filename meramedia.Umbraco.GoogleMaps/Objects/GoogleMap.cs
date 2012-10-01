5fe167fdd61a7f365d2883b6e58a1344283f2457
Nicklas Gavelin <nicklas@meramedia.se>
2012-10-01 17:01:13 +0200

    Missing "https" and zoom attribute for iframe url

--- meramedia.Umbraco.GoogleMaps/Objects/GoogleMap.cs
+++ meramedia.Umbraco.GoogleMaps/Objects/GoogleMap.cs
@@ -69,11 +69,13 @@ namespace meramedia.Umbraco.GoogleMaps.Objects
 
         /// <summary>
         /// Returns a simple iframe URL
+        /// 
+        /// Ex. https://maps.google.se/maps?q=65.58433540994488,22.151107191170013&amp;ie=UTF8&amp;t=m&amp;z=14&amp;&amp;output=embed
         /// </summary>
         /// <returns>Iframe url, not the iframe object</returns>
         public String GetIframeUrl( String center = null )
         {
-            return "http://maps.google.se/maps?q=" + ( !String.IsNullOrEmpty( center ) ? center : Options.Center ) + "&amp;iwloc=near&amp;output=embed";
+            return "https://maps.google.se/maps?q=" + ( !String.IsNullOrEmpty( center ) ? center : Options.Center ) + "&amp;ie=UTF8&amp;t=m&amp;z=" + Options.Zoom + "&amp;&amp;output=embed";
         }
 
         /// <summary>
